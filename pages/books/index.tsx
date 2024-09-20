f2dde80d981ca81601bd602f8c8407780bb1eebd
ward-ucll <113981513+ward-ucll@users.noreply.github.com>
2024-09-20 22:18:55 +0200

    kleine dingen veranderd voor pagination buttons

--- pages/books/index.tsx
+++ pages/books/index.tsx
@@ -1,36 +1,35 @@
-import BookItem from '@/components/Book/BookItem';
-import BookOverview from '@/components/Book/BookOverview';
-import Button from '@/components/General/Buttons/Button';
-import Header from '@/components/Header/Header';
-import { getAllBooks } from '@/services/bookservice';
-import React, { useState } from 'react';
-import useSWR from 'swr';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-
+import BookItem from "@/components/Book/BookItem";
+import BookOverview from "@/components/Book/BookOverview";
+import Button from "@/components/General/Buttons/Button";
+import Header from "@/components/Header/Header";
+import { getAllBooks } from "@/services/bookservice";
+import React, { useState } from "react";
+import useSWR from "swr";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const getBooks = async () => {
-    const response = await getAllBooks();
-    const data = await response.json()
-    return { data }
-} 
+  const response = await getAllBooks();
+  const data = await response.json();
+  return { data };
+};
 
 const BooksPage: React.FC = () => {
-    const [isListView, setIsListView] = useState(false);
-    
-    const {data, error, isLoading} = useSWR("api/book/all", getBooks);
-    const toggleView = () => {
-        setIsListView(!isListView);
-    };
+  const [isListView, setIsListView] = useState(false);
 
-    const handleButtonClick = () => {
-        console.log("Button clicked")
-    }
+  const { data, error, isLoading } = useSWR("api/book/all", getBooks);
+  const toggleView = () => {
+    setIsListView(!isListView);
+  };
 
-    return (
-        <div>
-            <Header></Header>
-            <div className="min-h-screen bg-gray-100 p-8">
+  const handleButtonClick = () => {
+    console.log("Button clicked");
+  };
+
+  return (
+    <div>
+      <Header></Header>
+      <div className="min-h-screen bg-gray-100 p-8">
         <div className="max-w-5xl m-auto">
           <div className="mb-5 mt-10 ">
             <button
@@ -45,16 +44,41 @@ const BooksPage: React.FC = () => {
               No books found
             </h1>
           )}
-            {( <BookOverview isListView={isListView} error={error}  books={data?.data.content} toggleView={toggleView}></BookOverview>)}
-            <Button handleButtonClick={handleButtonClick} label=""><ArrowBackIcon /></Button>
-            <Button handleButtonClick={handleButtonClick} label="1"></Button>
-            <Button handleButtonClick={handleButtonClick} label="5"></Button>
-            <Button handleButtonClick={handleButtonClick} label="" ><ArrowForwardIcon /></Button>
-          </div>
-          <svg data-testid="ArrowForward"></svg>
+          {
+            <BookOverview
+              isListView={isListView}
+              error={error}
+              books={data?.data.content}
+              toggleView={toggleView}
+            ></BookOverview>
+          }
+          <div className="pt-10 flex justify-center items-center space-x-4 h-full">
+            {data?.data.page.number > 0 && (
+              <Button handleButtonClick={handleButtonClick} label="">
+                <ArrowBackIcon />
+              </Button>
+            )}
+            {data && (
+              <>
+                <Button
+                  handleButtonClick={handleButtonClick}
+                  label="1"
+                ></Button>
+                <Button
+                  handleButtonClick={handleButtonClick}
+                  label={data.data.page.totalPages}
+                ></Button>
+                <Button handleButtonClick={handleButtonClick} label="">
+                  <ArrowForwardIcon />
+                </Button>
+              </>
+            )}
           </div>
         </div>
-    );
+        <svg data-testid="ArrowForward"></svg>
+      </div>
+    </div>
+  );
 };
 
 export default BooksPage;
